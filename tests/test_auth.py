e1870ca0385a26c844b09e58a3de0182ff635aab
Ogollah <kogollah2011@gmail.com>
2018-03-05 23:13:56 +0300

    [ch + #155637023] add user registration test

---
+++ tests/test_auth.py
@@ -0,0 +1,18 @@
+#app/tests/test_auth.py
+import unittest
+import json
+from app import app
+
+#authentication testcases
+class AuthTestCase(unittest.TestCase):
+    
+    #test user registration
+    def test_user_registration(self):
+        user = Users()
+        response = user.register_user()
+            # convert the json response to an object
+        result = json.loads(response.data.decode())
+            # the user should be successfully registered
+        self.assertEqual(result['message'],
+                             'You have registered successfully!')
+        self.assertEqual(response.status_code, 201)
